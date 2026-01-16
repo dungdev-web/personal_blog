d8acf0cecc9f0951bdbccc277a2a24ea18092f85
LƯU ĐỨC DŨNG <dung.dev.web@gmail.com>
2026-01-16 11:34:01 +0700

    Update firebase.js

--- personal-blog/src/firebase/firebase.js
+++ personal-blog/src/firebase/firebase.js
@@ -2,13 +2,12 @@ import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAaTl_YlwYVKwYDxxByMVTPx9KT4epkevg",
-  authDomain: "fir-b4383.firebaseapp.com",
-  projectId: "fir-b4383",
-  storageBucket: "fir-b4383.firebasestorage.app",
-  messagingSenderId: "691868026276",
-  appId: "1:691868026276:web:bb61db5539657c2d4cad31",
-  measurementId: "G-KYYEHHF2ET"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig)
